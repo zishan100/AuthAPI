fd24c08951546f0f0c7b4913a230cf7b7897f0f5
zishan100 <zishanansari111@gmail.com>
2022-10-25 13:01:18 +0530

    Changes db url

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   PORT: process.env.PORT || 8033,
   MONGO_DB:
     process.env.MONGODB_ATLAS ||
-    "mongodb://testUser:testPass@10.2.2.142:27017/testDB?authSource=admin",
+    "mongodb://testUser:testPass@10.2.2.142:27017/nashDB?authSource=admin",
   ACCESS_TOKEN_KEY:
     process.env.ACCESS_TOKEN_KEY ||
     "f8d2c69034c0031bd513e99b4010351e2b94fa4626c89a56f58146ceb3cb61024adb01247e2f28a4fcbe0c9541a1017f474e6bed25e126eb167f7e75c76ecd52",
